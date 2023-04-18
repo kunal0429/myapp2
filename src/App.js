40a5e3263b89d02a23cbf4472de85e00fc38612c
kunal0429 <kunalkk1997@gmail.com>
2023-04-18 11:35:42 +0530

    addition

--- src/App.js
+++ src/App.js
@@ -4,20 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1> Hello this is my first web page that I deployed on Github</h1>\
+     <a src="https://www.google.com"> Click on that to reach out to google  </a>
     </div>
   );
 }
