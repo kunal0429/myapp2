4ee71300a2c8d75dd6a857ee16420d0656a4806d
kunal0429 <kunalkk1997@gmail.com>
2023-04-19 12:48:08 +0530

    img added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+    <img src="/assets/dragon.jpg" alt ="dragon" width="200" height="100"></img>
      <h1> Hello this is my first web page that I deployed on Github</h1>
      <a href="https://www.google.com"> Click on that to reach out to google  </a>
     </div>
